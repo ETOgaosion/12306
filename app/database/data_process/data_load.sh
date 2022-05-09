9d9020d5123edfad1ca3084a091bd368ab7cc931
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2022-05-09 22:08:49 +0800

    data_load.sh: fix

--- app/database/data_process/data_load.sh
+++ app/database/data_process/data_load.sh
@@ -8,10 +8,10 @@ rm -f data_load.sql
 
 # gen sql
 touch data_load.sql
-echo "copy train(t_train_id, t_train_type, t_train_name) from '$train_path' delimiter ',' csv header;" >> data_load.sql
-# echo "copy city(c_city_id, c_city_name, c_reach_table) from '$city_path' delimiter ',' csv header;" >> data_load.sql
-echo "copy station_list(s_station_id, s_station_name, s_station_city_id) from '$station_list_path' delimiter ',' csv header;" >> data_load.sql
-# echo "copy train_full_info(tfi_train_id, tfi_station_id, tfi_station_order, tfi_arrive_time, tfi_leave_time, tfi_day_from_departure, tfi_distance, tfi_price) from '$train_path' delimiter ',' csv header;" >> data_load.sql
+echo "\\copy train(t_train_id, t_train_type, t_train_name) from '$train_path' delimiter ',' csv header;" >> data_load.sql
+echo "\\copy city(c_city_id, c_city_name, c_reach_table) from '$city_path' delimiter ',' csv header;" >> data_load.sql
+echo "\\copy station_list(s_station_id, s_station_name, s_station_city_id) from '$station_list_path' delimiter ',' csv header;" >> data_load.sql
+echo "\\copy train_full_info(tfi_train_id, tfi_station_id, tfi_station_order, tfi_arrive_time, tfi_leave_time, tfi_day_from_departure, tfi_distance, tfi_price) from '$train_full_info_path' delimiter ',' csv header;" >> data_load.sql
 
 # database name to be confirmed
 psql -f data_load.sql -U gzy -d postgres
